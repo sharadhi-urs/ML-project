729a17b3e65fbd6c71b4ecc274cfd4a44405d474
Sharadhi Urs <sharadhiurs@yahoo.com>
2024-03-12 21:12:35 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requiremnets
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        [req.replace("\n","")for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+            
+    return requirements
+
+
+setup(
+name='Machine Learning Project',
+version='0.0.1',
+author='Sharadhi Urs',
+author_email='shardhiurs@yahoo.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt') 
+)
